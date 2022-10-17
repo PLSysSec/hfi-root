18483fd0424b695b82fb70a742a41a0ed460b844
Joey Rudek <jrudek@ucsd.edu>
2022-10-17 16:32:01 -0700

    gem5 runscript now uses SightglassBench.inc

--- hfi-start-gem5-run.sh
+++ hfi-start-gem5-run.sh
@@ -1,10 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 if [ $# -ne 1 ]; then
-    echo "usage: ./hfi-start-run.sh <mode>, where mode is guardpage_asmmove, hfi, or hfiemulate"
+    echo "usage: ./hfi-start-run.sh <mode>, where mode is guardpage_asmmove, hfi, or hfiemulate2"
     exit 1
 fi
-if [ "$1" != "guardpage_asmmove" ] && [ "$1" != "hfi" ] && [ "$1" != "hfiemulate" ]; then
-    echo "usage: ./hfi-start-run.sh <mode>, where mode is guardpage_asmmove, hfi, or hfiemulate"
+if [ "$1" != "guardpage_asmmove" ] && [ "$1" != "hfi" ] && [ "$1" != "hfiemulate2" ]; then
+    echo "usage: ./hfi-start-run.sh <mode>, where mode is guardpage_asmmove, hfi, or hfiemulate2"
     exit 1
 fi
 MODE=$1
@@ -19,8 +19,11 @@ mkdir -p $BMKSDIR
 # assume that the user ran `make bootstrap` already and that everything is built
 
 # goal: build bmks and construct a command which will run each benchmark in parallel thru gem5
-BMKS="meshoptimizer shootout-ackermann shootout-gimli shootout-minicsv shootout-sieve shootout-base64 shootout-heapsort shootout-nestedloop pulldown-cmark shootout-ctype shootout-keccak shootout-random shootout-switch shootout-ed25519 shootout-matrix shootout-ratelimit shootout-xblabla20 bz2 shootout-fib2 shootout-memmove shootout-seqhash shootout-xchacha20"
-bmk="blake3-scalar"  # one extra benchmark, since the assignment to run_bmks_cmd is different at first
+BMKS=$(cat $HFIROOT/hfi-sightglass/mybuild/SightglassBench.inc)
+BMKS_ARR=($BMKS)
+bmk=${BMKS_ARR[0]}  # one extra benchmark, since the assignment to run_bmks_cmd is different at first
+unset BMKS_ARR[0]
+BMKS=$(echo ${BMKS_ARR[@]})
 
 # for each benchmark, build only the correct config
 cd ${HFIROOT}/hfi-sightglass/mybuild
@@ -37,5 +40,6 @@ done
 
 # finally, run the command
 cd ../benchmarks
+
 eval $run_bmks_cmd
 
