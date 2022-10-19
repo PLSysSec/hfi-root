7532f2610843590279270f99dc1fa2e622dbdfc7
Joey Rudek <jrudek@ucsd.edu>
2022-10-18 22:55:58 -0700

    pdfcrop

--- benchmarks/plot_run.sh
+++ benchmarks/plot_run.sh
@@ -17,8 +17,10 @@ cd $RESULTS
 ../make_results_files.sh
 if [[ $RESULTS == *"simulate"* ]]; then
 	gnuplot ../barchart-gem5.plot
+	pdfcrop $RESULTS/output-gem5.pdf $RESULTS/output-gem5.pdf
 elif [[ $RESULTS == *"emulate"* ]]; then
 	gnuplot ../barchart-native.plot
+	pdfcrop $RESULTS/output.pdf $RESULTS/output.pdf
 else
 	echo "Results directory doesn't have 'simulate' or 'emulate' in name; not sure which script to use."
 	echo "cd into $RESULTS and run gnuplot with the appropriate barchart-*.plot."
